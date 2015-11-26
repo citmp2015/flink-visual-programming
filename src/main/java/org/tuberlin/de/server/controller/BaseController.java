8e8cd39d2f4e22e45d150bb0499235358c58602c
Adrian Bartnik <adrian.bartnik@web.de>
2015-11-26 22:15:32 +0100

    Added Slf4j Logger

--- src/main/java/org/tuberlin/de/server/controller/BaseController.java
+++ src/main/java/org/tuberlin/de/server/controller/BaseController.java
@@ -1,18 +1,40 @@
 package org.tuberlin.de.server.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /**
- * Created by Fabian on 25.11.2015.
+ * Base controller for testing purposes. Demonstrates basic functionality and logging.
  */
+@WebServlet(urlPatterns ={ "/BaseController" })
 public class BaseController extends HttpServlet {
 
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws
-		  ServletException, IOException {
-    resp.sendRedirect("/pages/index.html");
-  }
+    private static final long serialVersionUID = 1L;
+    private static final Logger LOG = LoggerFactory.getLogger(BaseController.class);
+
+    @Override
+    public void init() throws ServletException {
+        LOG.debug("Init");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws
+            ServletException, IOException {
+        LOG.debug("DoGet");
+
+        resp.sendRedirect("/pages/index.html");
+    }
+
+    @Override
+    public void destroy() {
+        LOG.debug("Destroy");
+    }
 }
