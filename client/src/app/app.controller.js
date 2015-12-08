bb347ac2132d13867f87386c557706edec9801ae
Markus Paeschke <markus.paeschke@gmail.com>
2015-12-08 23:23:42 +0100

    Finalized first version of the drag and drop functionality.

--- client/src/app/app.controller.js
+++ client/src/app/app.controller.js
@@ -9,24 +9,19 @@
     /*@ngInject*/
     function AppCtrl($scope, $rootScope) {
 
-				$scope.graph = new joint.dia.Graph;
+		$scope.graph = new joint.dia.Graph;
 
         $scope.onDropComplete = function(data, evt) {
-            console.log($scope);
-            $scope.graph.addCells([$scope.renderNumberFilter(evt.x, evt.y)]);
+            // TODO: replace static sizes
+            var posX = evt.x - 250,
+                posY = evt.y - 51;
+                console.log(posX, posY);
+            if (data.type === 'stringFilter') {
+                $scope.graph.addCells([$scope.renderStringFilter(posX, posY)]);
+            } else if (data.type === 'numberFilter') {
+                $scope.graph.addCells([$scope.renderNumberFilter(posX, posY)]);
+            }
         };
-
-        function renderStringFilter(posX, posY) {
-            var rect = new joint.shapes.basic.Rect({
-                position: { x: posX, y: posY },
-                size: { width: 100, height: 30 },
-                attrs: {
-                    rect: { fill: 'green' },
-                    text: { text: 'String Filter', fill: 'white' } }
-            });
-
-            return rect;
-        }
 
     }
 
