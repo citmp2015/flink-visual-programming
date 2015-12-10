61cada52f8e5f2f18b9281b926fa753e845f8c64
MonsJovis <markus.aurich@gmail.com>
2015-12-10 14:53:27 +0100

    remove the the two rectangulars in the graph

--- client/src/directive/joint-diagram.js
+++ client/src/directive/joint-diagram.js
@@ -38,40 +38,6 @@
                 model: scope.graph,
             });
 
-            var rect = new joint.shapes.basic.Rect({
-                position: {
-                    x: 100,
-                    y: 30
-                },
-                size: {
-                    width: 100,
-                    height: 30
-                },
-                attrs: {
-                    rect: {
-                        fill: 'blue'
-                    },
-                    text: {
-                        text: 'my box',
-                        fill: 'white'
-                    }
-                }
-            });
-
-            var rect2 = rect.clone();
-            rect2.translate(300);
-
-            var link = new joint.dia.Link({
-                source: {
-                    id: rect.id
-                },
-                target: {
-                    id: rect2.id
-                }
-            });
-
-            scope.graph.addCells([rect, rect2, link]);
-
             return paper;
         }
 
