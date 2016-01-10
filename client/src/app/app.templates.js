180934535c61f90f2923ba0e6e6ebf268b518cfa
Markus Paeschke <markus.paeschke@gmail.com>
2016-01-10 14:16:09 +0100

    Fixed an error which occurs during the grunt uglify task because of the accent instead an apostrophe.

--- client/src/app/app.templates.js
+++ client/src/app/app.templates.js
@@ -18,15 +18,15 @@
             for (var index = 1; index < columns.length; index++) {
                 types = types + ',' + columns[index].type.label;
             }
-            return `DataSet<${types}> text = env.readTextFile("${path}");`;
+            return 'DataSet<' + types + '> text = env.readTextFile("' + path + '");';
         };
 
         template.createTextDatasourceTemplate = function(path) {
-            return `DataSet<String> text = env.readTextFile("${path}");`;
+            return 'DataSet<String> text = env.readTextFile("' + path + '");';
         };
 
         template.createNumberFilterTemplate = function(operator, value) {
-            return `data.filter(new FilterFunction<Integer>() {\n  public boolean filter(Integer value) {\n   return value ${operator} ${value};\n }\n});`;
+            return 'data.filter(new FilterFunction<Integer>() {\n  public boolean filter(Integer value) {\n   return value ${operator} ${value};\n }\n});';
         };
 
         template.createFlatMapTemplate = function() {
