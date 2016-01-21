5c9c8238321f5762e191d2669b4a334b97dd1721
Markus Paeschke <markus.paeschke@gmail.com>
2016-01-21 22:10:04 +0100

    Added functionality to store the general settings in the local storage.

--- client/src/app/generalsettings/generalsettings-modal.controller.js
+++ client/src/app/generalsettings/generalsettings-modal.controller.js
@@ -7,12 +7,19 @@
         .controller('generalsettingsModalCtrl', GeneralsettingsModalCtrl);
 
     /*@ngInject*/
-    function GeneralsettingsModalCtrl($scope, $rootScope, $uibModalInstance, $stateParams, $timeout, graphFactory, $log) {
+    function GeneralsettingsModalCtrl($scope, $rootScope, $uibModalInstance, $stateParams, $timeout, localStorageService, $log) {
+
+        var config = localStorageService.get('config') || {};
+        $scope.generalsettings = {
+            flinkURL: config.flinkURL || '',
+            flinkPort: config.flinkPort || ''
+        };
 
         $scope.save = save;
         $scope.cancel = cancel;
 
         function save() {
+            localStorageService.set('config', $scope.generalsettings);
             $uibModalInstance.close();
         }
 
