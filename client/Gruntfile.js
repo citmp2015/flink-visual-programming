978e07609bad21c98f79f90b0c3d449abdb52312
Iwailo Denisow <iwailodenisow@mail.tu-berlin.de>
2016-01-27 22:10:49 +0100

    Remve unused dummy files

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -380,11 +380,6 @@ module.exports = function(grunt) {
                     cwd: 'bower_components/components-bootstrap/fonts',
                     dest: '<%= flinkVisual.dist %>/fonts',
                     src: ['*']
-                }, {
-                    expand: true,
-                    cwd: '<%= flinkVisual.app %>/dummydata',
-                    dest: '<%= flinkVisual.dist %>/dummydata',
-                    src: ['*']
                 }]
             }
         }
