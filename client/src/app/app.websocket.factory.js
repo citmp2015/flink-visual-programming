294a49a3c587e6b1842116c21cb2cf6f10850f5b
MonsJovis <markus.aurich@gmail.com>
2016-02-16 13:04:34 +0100

    fix eventkey parsing

--- client/src/app/app.websocket.factory.js
+++ client/src/app/app.websocket.factory.js
@@ -15,12 +15,11 @@
         var dataStream = $websocket(domain.replace('http://', 'ws://') + '/ControllerWebSocket');
         var scope;
 
-        dataStream.onMessage(function(message) {
-            var eventKey = message;
-            var data = null;
-            if (eventKey.indexOf(' ')) {
+        dataStream.onMessage(function(e) {
+            var data, eventKey = e.data;
+            if (eventKey.indexOf(' ') !== -1) {
                 eventKey = eventKey.substr(0, eventKey.indexOf(' '));
-                data = message.substr(message.indexOf(' '));
+                data = e.data.substr(e.data.indexOf(' '));
             }
             $log.debug(eventKey, data);
             if (scope && scope.$broadcast) {
